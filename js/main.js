ff4dda0305c920c288bc0d3c20e2cf24c89e190f
gitconfigMOUMENdesk <ourmoumen@gmail.com>
2022-09-11 19:31:03 +0100

    i add the therd page style it and animat it

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const btn1 = document.getElementsByClassName('.arowbtn')
 const btn2 =document.getElementsByClassName('.arowbtn.splash1')
+const btn3 =document.getElementsByClassName('.arowbtn.splash2')
 function next() {
     window.location.href = "../serviceKoom/pages/firstpage.html"
     console.log("dsds")
 }
 function next2() {
     window.location.href ="../pages/secondpage.html"
+    console.log("dsds")
+}
+function next3() {
+    window.location.href ="../pages/theredpage.html"
     console.log("dsds")
  }
