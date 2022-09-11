d9b3676ee70ccc22c30811645a3e2845545938ab
gitconfigMOUMENdesk <ourmoumen@gmail.com>
2022-09-11 17:14:58 +0100

    i add function and style to ther second page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-const btn = document.getElementsByClassName('.arowbtn')
-
+const btn1 = document.getElementsByClassName('.arowbtn')
+const btn2 =document.getElementsByClassName('.arowbtn.splash1')
 function next() {
     window.location.href = "../serviceKoom/pages/firstpage.html"
+    console.log("dsds")
+}
+function next2() {
+    window.location.href ="../pages/secondpage.html"
     console.log("dsds")
  }
